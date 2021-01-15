d43b236973963000b5a3fa21728cc0c8b1aefa28
cobra21 <zayczev.artur@yandex.ru>
2021-01-15 10:19:49 +0500

    pblock

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import { Route } from 'react-router-dom';
-import { Header } from './components';
-import { Home, Cart } from './pages';
+import React from "react";
+import { Route } from "react-router-dom";
+import { Header } from "./components";
+import { Home, Cart } from "./pages";
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("http://localhost:3000/db.json")
+      .then(res => res.json())
+      .then(json => {
+        console.log(json.pizzas);
+        setPizzas(json.pizzas);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route path="/" component={Home} exact />
+        <Route path="/" render={() => <Home items={pizzas} />} exact />
         <Route path="/cart" component={Cart} exact />
       </div>
     </div>
